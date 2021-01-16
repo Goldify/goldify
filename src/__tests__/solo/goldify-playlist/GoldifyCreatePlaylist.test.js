ea20ed52bf1dfb6b06a81067cffa74ab19b9ac85
Matt Grant <mgrant@sdocs.com>
2021-01-16 03:25:23 -0500

    Replaces Goldify Playlist creation alerts with a popup the user must click through

--- src/__tests__/solo/goldify-playlist/GoldifyCreatePlaylist.test.js
+++ src/__tests__/solo/goldify-playlist/GoldifyCreatePlaylist.test.js
@@ -34,75 +34,45 @@ const createdPlaylist = playlistFixtures.createGoldifyPlaylist(
   GOLDIFY_PLAYLIST_DESCRIPTION
 );
 
-test("Test GoldifyCreatePlaylist with and without required props", async () => {
-  const wrapper = shallow(
+const createGoldifyPlaylistWrapper = () => {
+  return shallow(
     <GoldifyCreatePlaylist
       retrievedTokenData={{}}
-      userData={{}}
+      userData={{
+        id: userInfoFixtures.getUserTestData().id,
+      }}
       playlistUpdater={jest.fn()}
     />
   );
-  wrapper.instance().createNewGoldifyPlaylist = jest.fn();
-  wrapper.instance().componentDidMount();
-  expect(wrapper.instance().createNewGoldifyPlaylist).not.toHaveBeenCalled();
-
-  wrapper.setProps({
-    retrievedTokenData: goldifySoloFixtures.getTokensTestData(),
-  });
-  wrapper.instance().componentDidMount();
-  expect(wrapper.instance().createNewGoldifyPlaylist).not.toHaveBeenCalled();
-
-  wrapper.setProps({
-    userData: userInfoFixtures.getUserTestData(),
-  });
-  wrapper.instance().componentDidMount();
-  expect(wrapper.instance().createNewGoldifyPlaylist).toHaveBeenCalledTimes(1);
-  expect(wrapper.instance().createNewGoldifyPlaylist).toHaveBeenCalledWith(
-    goldifySoloFixtures.getTokensTestData(),
-    userInfoFixtures.getUserTestData().id
-  );
-});
+};
 
 test("Test functionality: createNewGoldifyPlaylist", async () => {
   createGoldifyPlaylist.mockImplementation(() =>
     Promise.resolve(createdPlaylist)
   );
   uploadPlaylistImage.mockImplementation(() => Promise.resolve(null));
-  jest.spyOn(window, "alert").mockImplementation(() => {});
 
-  const wrapper = shallow(
-    <GoldifyCreatePlaylist
-      retrievedTokenData={{}}
-      userData={{}}
-      playlistUpdater={jest.fn()}
-    />
-  );
+  const wrapper = createGoldifyPlaylistWrapper();
+  wrapper.instance().handlePlaylistCreated = jest.fn();
   await wrapper
     .instance()
     .createNewGoldifyPlaylist(
       goldifySoloFixtures.getTokensTestData(),
       userInfoFixtures.getUserTestData().id
     );
-  expect(alert).toHaveBeenCalledTimes(1);
-  // Upload image after playlist is created
-  expect(uploadPlaylistImage).toHaveBeenCalledTimes(1);
-  expect(wrapper.instance().props.playlistUpdater).toHaveBeenCalledTimes(1);
-  expect(wrapper.instance().props.playlistUpdater).toHaveBeenCalledWith(
+  expect(wrapper.instance().handlePlaylistCreated).toHaveBeenCalledTimes(1);
+  expect(wrapper.instance().handlePlaylistCreated).toHaveBeenCalledWith(
     createdPlaylist
   );
+  // Upload image after playlist is created
+  expect(uploadPlaylistImage).toHaveBeenCalledTimes(1);
 });
 
 test("Expect alert, home page when running createGoldifyPlaylist with bad data", async () => {
   createGoldifyPlaylist.mockImplementation(() => Promise.resolve(null));
   jest.spyOn(window, "alert").mockImplementation(() => {});
 
-  const wrapper = shallow(
-    <GoldifyCreatePlaylist
-      retrievedTokenData={{}}
-      userData={{}}
-      playlistUpdater={jest.fn()}
-    />
-  );
+  const wrapper = createGoldifyPlaylistWrapper();
   await wrapper
     .instance()
     .createNewGoldifyPlaylist(
@@ -117,15 +87,92 @@ test("Expect alert, home page when running createGoldifyPlaylist with bad data",
 });
 
 test("Check for which div is loaded on render for createGoldifyPlaylist", () => {
-  const wrapper = shallow(
-    <GoldifyCreatePlaylist
-      retrievedTokenData={{}}
-      userData={{}}
-      playlistUpdater={jest.fn()}
-    />
-  );
+  const wrapper = createGoldifyPlaylistWrapper();
   wrapper.instance().createPlaylistDiv = jest
     .fn()
     .mockReturnValue("Creating Goldify Playlist!");
   expect(wrapper.instance().render()).toEqual("Creating Goldify Playlist!");
+});
+
+test("Check for elements based on loading and success state", () => {
+  const wrapper = createGoldifyPlaylistWrapper();
+
+  wrapper.instance().state = {
+    loading: false,
+    success: false,
+  };
+  let createPlaylistDivString = JSON.stringify(
+    wrapper.instance().createPlaylistDiv()
+  );
+  expect(createPlaylistDivString).toContain("Create Goldify Playlist");
+
+  wrapper.instance().state = {
+    loading: true,
+    success: false,
+  };
+  createPlaylistDivString = JSON.stringify(
+    wrapper.instance().createPlaylistDiv()
+  );
+  expect(createPlaylistDivString).not.toContain("Create Goldify Playlist");
+
+  wrapper.instance().state = {
+    loading: false,
+    success: true,
+  };
+  createPlaylistDivString = JSON.stringify(
+    wrapper.instance().createPlaylistDiv()
+  );
+  expect(createPlaylistDivString).not.toContain("Create Goldify Playlist");
+});
+
+test("Confirm createNewGoldifyPlaylistAction calls createNewGoldifyPlaylist", () => {
+  const wrapper = createGoldifyPlaylistWrapper();
+  wrapper.instance().createNewGoldifyPlaylist = jest.fn();
+  wrapper.instance().createNewGoldifyPlaylistAction();
+  expect(wrapper.instance().createNewGoldifyPlaylist).toHaveBeenCalledTimes(1);
+  expect(wrapper.instance().createNewGoldifyPlaylist).toHaveBeenCalledWith(
+    {},
+    userInfoFixtures.getUserTestData().id
+  );
+});
+
+test("Confirm handleButtonClick sets state to loading, then calls createNewGoldifyPlaylistAction", () => {
+  const wrapper = createGoldifyPlaylistWrapper();
+  wrapper.instance().setState = jest.fn();
+  wrapper.instance().createNewGoldifyPlaylistAction = jest.fn();
+
+  wrapper.instance().state.loading = true;
+  wrapper.instance().handleButtonClick();
+  expect(wrapper.instance().setState).not.toHaveBeenCalled();
+  expect(
+    wrapper.instance().createNewGoldifyPlaylistAction
+  ).not.toHaveBeenCalled();
+
+  wrapper.instance().state.loading = false;
+  wrapper.instance().handleButtonClick();
+  expect(wrapper.instance().setState).toHaveBeenCalledTimes(1);
+  expect(wrapper.instance().setState).toHaveBeenCalledWith({
+    loading: true,
+    success: false,
+  });
+  expect(
+    wrapper.instance().createNewGoldifyPlaylistAction
+  ).toHaveBeenCalledTimes(1);
+});
+
+test("Confirm handlePlaylistCreated calls correct window timeouts and state is set to success", () => {
+  jest.useFakeTimers();
+  const wrapper = createGoldifyPlaylistWrapper();
+  wrapper.instance().setState = jest.fn();
+  wrapper.instance().handlePlaylistCreated({});
+  jest.advanceTimersByTime(2000);
+  expect(wrapper.instance().setState).toHaveBeenCalledTimes(1);
+  expect(wrapper.instance().setState).toHaveBeenCalledWith({
+    loading: false,
+    success: true,
+  });
+  expect(wrapper.instance().props.playlistUpdater).not.toHaveBeenCalled();
+  jest.advanceTimersByTime(1500);
+  expect(wrapper.instance().props.playlistUpdater).toHaveBeenCalledTimes(1);
+  expect(wrapper.instance().props.playlistUpdater).toHaveBeenCalledWith({});
 });
