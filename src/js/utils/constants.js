3a22f4c098bd3cdd761165a5988c6b6436598c97
Matt Grant <mgrant@sdocs.com>
2021-03-06 00:42:26 -0500

    Updates description of generated Goldify playlist

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,6 +1,6 @@
 export const GOLDIFY_PLAYLIST_NAME = "Goldify";
 export const GOLDIFY_PLAYLIST_DESCRIPTION =
-  "Would you look at that, it's a Goldify testing playlist! Additional features coming soon...";
+  "Playlist powered by Goldify. Visit goldify.app to start your own!";
 export const HOME_PAGE_PATH = "/";
 export const SOLO_PAGE_PATH = "/goldify";
 export const shortTermTracksRecommended = 2;
