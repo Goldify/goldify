ffd5a00f6c2fb794e2d146e0d1b6b62871cda250
Matt Grant <mgrant@sdocs.com>
2021-03-06 01:18:20 -0500

    Update /goldify to /solo to reflect Solo feature

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -2,7 +2,7 @@ export const GOLDIFY_PLAYLIST_NAME = "Goldify";
 export const GOLDIFY_PLAYLIST_DESCRIPTION =
   "Playlist powered by Goldify. Visit goldify.app to start your own!";
 export const HOME_PAGE_PATH = "/";
-export const SOLO_PAGE_PATH = "/goldify";
+export const SOLO_PAGE_PATH = "/solo";
 export const shortTermTracksRecommended = 2;
 export const mediumTermTracksRecommended = 2;
 export const longTermTracksRecommended = 2;
